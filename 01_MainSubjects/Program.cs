9926c12dcf4788bf3b1655c4b65583266c0d8c2c
WARRIORDAG <hasandag.gtr@gmail.com>
2024-10-05 10:46:30 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1 YAZDIRMA KOMUTLARI // CSharp eğitim kampı 1.ders
+            // bu şekilde slash ları kullnadığımda comment olarak yazılır
+            // Write ve WriteLine arasındaki fark,
+            //Console.Write("hello world");       // hello world yazdırdım imlec alt satıra inmedi
+
+            //Console.WriteLine("\nhi guys!!");     // imleci yazdırdıktan sonra alt satıra indiriyor öncesinde imleç alt satıra insin diye \yaptım
+
+            //Console.WriteLine("***** Yemek kategorileri *****");
+            //Console.WriteLine();
+            //Console.WriteLine("1- Çorba");
+            //Console.WriteLine("2- Ana Yemkler Kebap");
+            //Console.WriteLine("3- Soğuk Başlangıçlar");
+            //Console.WriteLine("4- Salatalar");
+            //Console.WriteLine("5- Tatlılar");
+            //Console.WriteLine("6- İçecekler");
+            //Console.WriteLine();            // boşluk bırakmak için kullanıyorum
+            //Console.WriteLine("***** Yemek kategorileri *****");
+
+
+            //Console.Read();   // ben enter tuşuna basana akdar konsolu bekletecek!!
+
+
+            #endregion
+            #region DEĞİŞKENLER  // CSharp eğitim kampı 1.ders
+
+            
+            
+            #endregion
+
+
+
+        }
+    }
+}
