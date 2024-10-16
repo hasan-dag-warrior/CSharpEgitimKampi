a2a747537eaeb047aadba836ed9cbbb1ba7bca63
WARRIORDAG <hasandag.gtr@gmail.com>
2024-10-16 20:36:04 +0300

    16.10.2024-1

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region YILDIZLARLA ŞEKİL ÇİZME 
+            // alt alta 10 tane yıldız oluşturma
+
+            //for (int i = 1; i <=10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+
+            // yan yana 10 tane yıldız oluşturma
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //    // Console.Write("**********");
+            //}
+
+            // alt alta her satırda 10 tane yıldız oluşturma
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //    // Console.Write("**********");
+            //}
+
+            // yıldızlarla dik üçgen oluşturma
+            //for(int i = 1; i<=5; i++)
+            //{
+            //    for (int j =1; j<= i; j++)
+            //    {
+            //        Console.Write("*");         // yan yana 1 er yıldız yazıyor!!
+            //    }
+            //    Console.WriteLine();
+            //} 
+
+            // yıldızlarla ters dik üögen oluşturma
+            //for (int i =5; i>=1; i--)
+            //{
+            //    for (int j = 1; j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            // yıldızlarla elmas şekli oluşturma
+
+
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
