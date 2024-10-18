91f282c32b302d2fabe47c1d20d0abb0f842e1b4
WARRIORDAG <hasandag.gtr@gmail.com>
2024-10-18 20:57:15 +0300

    18.10.2024-2

--- 05_LoopsWithStars/Program.cs
+++ 05_LoopsWithStars/Program.cs
@@ -34,27 +34,124 @@ namespace _05_LoopsWithStars
             //}
 
             // yıldızlarla dik üçgen oluşturma
-            //for(int i = 1; i<=5; i++)
+            //for(int i = 1; i<=5; i++)         // satırları kontrol etmek için i yi atadım
             //{
-            //    for (int j =1; j<= i; j++)
+            //    for (int j =1; j<= i; j++)        // sütunları kontrol etmek için j yi yazdım
             //    {
             //        Console.Write("*");         // yan yana 1 er yıldız yazıyor!!
             //    }
             //    Console.WriteLine();
             //} 
 
-            // yıldızlarla ters dik üögen oluşturma
-            //for (int i =5; i>=1; i--)
+            // yıldızlarla ters dik üçgen oluşturma
+            //for (int i = 5; i >= 1; i--)     // satırlar için i atadım
             //{
-            //    for (int j = 1; j<=i; j++)
+            //    for (int j = 1; j <= i; j++)        // sütunlar için j atadım
             //    {
             //        Console.Write("*");
             //    }
             //    Console.WriteLine();
             //}
 
-            // yıldızlarla elmas şekli oluşturmaa
+            #region dik ve ters dik üçgen alt alta
 
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(" *");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int k = 4; k >= 1; k--)
+            //{
+            //    for (int m = 1; m <= k; m++)
+            //    {
+            //        Console.Write(" *");
+
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+
+            #endregion
+
+            #region BAKLAVA DİLİMİ
+
+            //int n = 10;      // yarı yükseklik değeri için tanımladım
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = n - 1; j > 0; j--)     // sol taraftan boşluk bırakıyor
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    n = n - 1;
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)    // yazdırılacak yıldız sayısını belirliyor!
+            //    {
+            //        Console.Write('*');
+            //    }
+
+            //    Console.WriteLine();
+            //    //n = n - 1;
+
+
+            //}
+
+            //// alt kısım 
+            //for (int i = n+1; i>=1; i--)  
+            //{
+            //    for (int j = n-1; j> 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    n = n+1;
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //    //n = n + 1;
+
+            //}
+
+
+
+
+
+
+            #endregion
+
+            #region  PİRAMİT
+
+            int n = 10;      // yarı yükseklik değeri için tanımladım
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = n - i; j > 0; j--)     // sol taraftan boşluk bırakıyor
+                {
+                    Console.Write(" ");
+                }
+                //n = n - 1;
+
+                for (int k = 1; k <= 2 * i - 1; k++)    // yazdırılacak yıldız sayısını belirliyor!
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+
+            }
+
+
+
+
+
+            #endregion
 
 
             #endregion
